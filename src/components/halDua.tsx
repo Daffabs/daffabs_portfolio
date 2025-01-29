3be7674ca6b9ce6fa0e714b7d903fcd5f03e8199
daffabs <daffabagus1933@gmail.com>
2025-01-29 19:39:19 +0700

    satu

--- src/components/halDua.tsx
+++ src/components/halDua.tsx
@@ -3,48 +3,76 @@ import React from "react";
 import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
 
-
 const HalDua = () => {
     return (
         <section className="h-auto">
-            <div className="grid-cols-1 sm:grid-cols-12">
+            <div className="grid grid-cols-1 sm:grid-cols-12">
                 <ThemeProvider
                     theme={{
                         palette: {
-                        primary: {
-                            main: '#FFA600',
-                            dark: '#CC8500',
-                        },
+                            primary: {
+                                main: '#FFA600',
+                                dark: '#CC8500',
+                            },
                         },
                     }}
-                    >
+                >
                     <Box
                         sx={{
-                        width: '100%',
-                        height: 50,
-                        bgcolor: 'transparent',
-                        background: 'linear-gradient(45deg, #3C3D37 0%, #A0A393  50%, #3C3D37 100% )',
-                        position: 'absolute',
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
+                            width: '100%',
+                            height: 50,
+                            bgcolor: 'transparent',
+                            background: 'linear-gradient(45deg, #3C3D37 0%, #A0A393 50%, #3C3D37 100% )',
+                            position: 'absolute',
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
                         }}
                     />
-                    </ThemeProvider>
+                </ThemeProvider>
             </div>
+
             <div className="flex flex-col items-center col-span-8 pt-6">
-                    <h1 className="text-white text-lg sm:text-2xl lg:leading-normal text-center">
-                        - Services -
-                    </h1>
-                    <p className="text-3xl sm:text-6xl col-span-8 sm:text-left justify-self-start pt-4 text-center">
-                        <span className="text-kuningNew-1 italic">Services</span>
-                        <span className="text-white"> I Provide</span>
+                <h1 className="text-white text-lg sm:text-2xl lg:text-4xl xl:text-5xl text-center">
+                    - Services -
+                </h1>
+                <p className="text-3xl sm:text-4xl lg:text-6xl xl:text-7xl pt-4 text-center">
+                    <span className="text-kuningNew-1 italic">Services</span>
+                    <span className="text-white"> I Provide</span>
+                </p>
+            </div>
+
+            <div className="flex flex-wrap justify-center sm:justify-between items-center gap-6 sm:gap-8 pt-10">
+                <Box
+                    sx={{
+                        width: { xs: '100%', sm: 250, md: 275, lg: 300 },
+                        height: 250,
+                        borderRadius: 5,
+                        bgcolor: '#ECDFCC',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                    className="mb-6 sm:mb-0"
+                >
+                    <img
+                        src="/images/uiux.png"
+                        alt="Deskripsi Gambar"
+                        style={{
+                            width: '40%',
+                            height: 'auto',
+                            borderRadius: '50%',
+                        }}
+                    />
+                    <p className="mt-3 text-dark-1 text-center font-bold text-lg sm:text-xl md:text-2xl">
+                        UI/UX Design
                     </p>
-                    </div>
-            <div className="flex flex-wrap justify-between items-center h-4/6 gap-4 pt-10">
-                    <Box
-                        sx={{
-                        width: { xs: '100%', sm: 250 },
+                </Box>
+
+                <Box
+                    sx={{
+                        width: { xs: '100%', sm: 250, md: 275, lg: 300 },
                         height: 250,
                         borderRadius: 5,
                         bgcolor: '#ECDFCC',
@@ -52,24 +80,26 @@ const HalDua = () => {
                         flexDirection: 'column',
                         justifyContent: 'center',
                         alignItems: 'center',
+                    }}
+                    className="mb-6 sm:mb-0"
+                >
+                    <img
+                        src="/images/frontend.png"
+                        alt="Deskripsi Gambar"
+                        style={{
+                            width: '40%',
+                            height: 'auto',
+                            borderRadius: '50%',
                         }}
-                        >
-                        <img
-                            src="/images/uiux.png"
-                            alt="Deskripsi Gambar"
-                            style={{
-                                width: '40%', 
-                                height: 'auto', 
-                                borderRadius: '50%',
-                            }}
-                        />
-                        <p style={{ marginTop: '10px', color: '#181C14', textAlign: 'center', fontWeight: 'bold', fontSize: '1.7rem' }}>
-                            UI/UX Design
-                        </p>
-                    </Box>
-                    <Box
-                        sx={{
-                        width: { xs: '100%', sm: 250 },
+                    />
+                    <p className="mt-3 text-dark-1 text-center font-bold text-lg sm:text-xl md:text-2xl">
+                        Frontend
+                    </p>
+                </Box>
+
+                <Box
+                    sx={{
+                        width: { xs: '100%', sm: 250, md: 275, lg: 300 },
                         height: 250,
                         borderRadius: 5,
                         bgcolor: '#ECDFCC',
@@ -77,49 +107,25 @@ const HalDua = () => {
                         flexDirection: 'column',
                         justifyContent: 'center',
                         alignItems: 'center',
+                    }}
+                    className="mb-6 sm:mb-0"
+                >
+                    <img
+                        src="/images/webdesign.png"
+                        alt="Deskripsi Gambar"
+                        style={{
+                            width: '40%',
+                            height: 'auto',
+                            borderRadius: '50%',
                         }}
-                        >
-                        <img
-                            src="/images/frontend.png"
-                            alt="Deskripsi Gambar"
-                            style={{
-                                width: '40%', 
-                                height: 'auto', 
-                                borderRadius: '50%',
-                            }}
-                        />
-                        <p style={{ marginTop: '10px', color: '#181C14', textAlign: 'center', fontWeight: 'bold', fontSize: '1.7rem' }}>
-                            Frontend
-                        </p>
-                    </Box>
-                    <Box
-                        sx={{
-                        width: { xs: '100%', sm: 250 },
-                        height: 250,
-                        borderRadius: 5,
-                        bgcolor: '#ECDFCC',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        }}
-                        >
-                        <img
-                            src="/images/webdesign.png"
-                            alt="Deskripsi Gambar"
-                            style={{
-                                width: '40%', 
-                                height: 'auto', 
-                                borderRadius: '50%',
-                            }}
-                        />
-                        <p style={{ marginTop: '10px', color: '#181C14', textAlign: 'center', fontWeight: 'bold', fontSize: '1.7rem' }}>
-                            Website Design
-                        </p>
-                    </Box>
-                    </div>
-            </section>
+                    />
+                    <p className="mt-3 text-dark-1 text-center font-bold text-lg sm:text-xl md:text-2xl">
+                        Website Design
+                    </p>
+                </Box>
+            </div>
+        </section>
     );
 };
 
-export default HalDua
+export default HalDua;
--- src/components/halSatu.tsx
+++ src/components/halSatu.tsx
@@ -6,66 +6,65 @@ import Link from "next/link";
 
 const HalSatu = () => {
     return (
-        <section className="h-screen flex items-center container ">
-            <div className="grid grid-cols-1 sm:grid-cols-12">
-            <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-            >
-            <h1 className="text-white mb-4 text-2xl sm:text-7xl lg:leading-normal font-extrabold">
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-white to-dark-3">
-                Hello, I&apos;m{" "}
-                </span>
-                <br />
-                    <TypeAnimation
-                    sequence={[
-                        "Daffa Bagus S P",
-                        1000,
-                        "Web Developer",
-                        1000,
-                        "Frontend Developer",
-                        1000,
-                        "UI/UX Designer",
-                        1000,
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    repeat={Infinity}
+        <section className="h-screen flex items-center container px-4">
+            <div className="grid grid-cols-1 sm:grid-cols-12 gap-8">
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="col-span-12 sm:col-span-8 place-self-center text-center sm:text-left justify-self-start"
+                >
+                    <h1 className="text-white mb-4 text-3xl sm:text-5xl lg:text-7xl font-extrabold">
+                        <span className="bg-clip-text text-transparent bg-gradient-to-r from-white to-dark-3">
+                            Hello, I&apos;m{" "}
+                        </span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                "Daffa Bagus S P",
+                                1000,
+                                "Web Developer",
+                                1000,
+                                "Frontend Developer",
+                                1000,
+                                "UI/UX Designer",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <div className="space-y-3 sm:space-y-4">
+                        <Link
+                            href="mailto:daffabagus1933@gmail.com"
+                            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-dark-4 to-dark-2 hover:bg-slate-200 text-dark-1 font-bold"
+                        >
+                            Hire Me
+                        </Link>
+                        <Link
+                            href="/CV_DAFFA BAGUS SYACH PUTRA.pdf"
+                            download
+                            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-dark-4 to-dark-2 hover:bg-slate-800 text-white font-bold"
+                        >
+                            <span className="block bg-dark-1 hover:bg-slate-800 rounded-full px-5 py-2">
+                                Download CV
+                            </span>
+                        </Link>
+                    </div>
+                </motion.div>
+                <div className="w-[250px] h-[250px] sm:w-[400px] sm:h-[400px] lg:w-[550px] lg:h-[550px] relative overflow-hidden col-span-12 sm:col-span-4 place-self-center mt-4 lg:mt-0">
+                    <Image
+                        src="/images/ppo.svg"
+                        alt="hero image"
+                        className="absolute inset-0 w-full h-full object-cover"
+                        width={400}
+                        height={400}
                     />
-            </h1>
-            <div>
-                <Link
-                href="mailto:daffabagus1933@gmail.com"
-                className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-dark-4 to-dark-2 hover:bg-slate-200 text-dark-1 font-bold"
-                >
-                Hire Me
-                </Link>
-                <Link
-                href="/CV_DAFFA BAGUS SYACH PUTRA.pdf"
-                download
-                className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-dark-4 to-dark-2 hover:bg-slate-800 text-white mt-3 font-bold"
-                >
-                <span className="block bg-dark-1 hover:bg-slate-800 rounded-full px-5 py-2">
-                    Download CV
-                </span>
-                </Link>
-            </div>
-            </motion.div>
-          <div className="w-[300px] h-[300px]
-          lg:w-[550px] lg:h-[550px] relative overflow-hidden col-span-4 place-self-center mt-4 lg:mt-0"> 
-            <Image
-              src="/images/ppo.svg"
-              alt="hero image"
-              className="absolute inset-0 w-full h-full object-cover" 
-              width={400} 
-              height={400} 
-            />
-          </div>
+                </div>
             </div>
         </section>
     );
 };
 
-export default HalSatu
+export default HalSatu;
